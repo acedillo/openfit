277f9c8c131554a1a77ecbca7585d98f9b0e68f5
Abraham Cedillo <abraham.cedillo@gm.com>
2021-03-05 10:21:14 -0500

    Added access points to next workout and image url fetch

--- app/src/main/java/com/acedillo/openfit/repository/RetrofitRepository.kt
+++ app/src/main/java/com/acedillo/openfit/repository/RetrofitRepository.kt
@@ -6,12 +6,20 @@ import com.acedillo.openfit.model.WorkoutDetail
 import com.acedillo.openfit.model.WorkoutImage
 import com.acedillo.openfit.rest.ApiClient
 import com.acedillo.openfit.rest.ApiInterface
+import com.google.gson.JsonSyntaxException
 
 class RetrofitRepository : Repository{
 
     override fun getWorkouts(): Main {
         val apiService = ApiClient.getClient().create(ApiInterface::class.java)
         val call = apiService.getWorkouts()
+        val response = call.execute()
+        return response.body()!!
+    }
+
+    override fun getNextWorkout(url : String): Main {
+        val apiService = ApiClient.getClient().create(ApiInterface::class.java)
+        val call = apiService.getNextWorkouts(url)
         val response = call.execute()
         return response.body()!!
     }
@@ -23,10 +31,14 @@ class RetrofitRepository : Repository{
         return response.body()!!
     }
 
-    override fun getThumbnail(id: Int): WorkoutImage {
+    override fun getThumbnail(id: Int): WorkoutImage? {
         val apiService = ApiClient.getClient().create(ApiInterface::class.java)
         val call = apiService.getThumbnail(id)
-        val response = call.execute()
-        return response.body()!!
+        return try {
+            val response = call.execute()
+            response.body()!!
+        }catch (e : JsonSyntaxException){
+            null
+        }
     }
 }
